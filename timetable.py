f288580d6e5e2ef33a28fd243fbb82fc8ad28731
zhuoyang <zhuoyang98@gmail.com>
2019-05-21 15:39:54 +0800

    auto determine sem duration, implement local usage

--- timetable.py
+++ timetable.py
@@ -4,26 +4,41 @@ import pathlib
 import datetime
 import csv
 import os
+import ptvsd
 from bs4 import BeautifulSoup
 
-cwd = os.path.dirname(os.path.abspath(__file__))
-page = urllib.request.urlopen("file:///" + cwd + "/" + sys.argv[1])
-soup = BeautifulSoup(page, 'html5lib')
+#VS Code debugger
+# print("Waiting for debugger attach")
+# sys.stdout.flush()
+# ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
+# ptvsd.wait_for_attach()
 
+if len(sys.argv) == 3:
+    if sys.argv[2] == "node":
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        page = urllib.request.urlopen("file:///" + cwd + "/" + sys.argv[1])
+else:
+    page = urllib.request.urlopen("file:///" + sys.argv[1])
+
+soup = BeautifulSoup(page, 'html5lib')
 allTable = soup.findAll('table', {'id': 'simple-table'})
 table = allTable[1].find('tbody').findAll('tr')
 
-semDate = soup.find('div', {'class': 'form-group'}).findAll('label')[1].contents[2].split(" ")[4]
-semDateTime = datetime.datetime.strptime(semDate, "%Y-%m-%d")
-
+# find how many study weeks in this sem
+semDate = soup.find('div', {'class': 'form-group'}).findAll('label')[1].contents[2].split(" ")
+semStartDate = semDate[4]
+semEndDate = semDate[7]
+semStartDateTime = datetime.datetime.strptime(semStartDate, "%Y-%m-%d")
+semEndDateTime = datetime.datetime.strptime(semEndDate, "%Y-%m-%d")
+semDuration = int(((semEndDateTime - semStartDateTime).days + 1) / 7) 
 dayDict = {
-    "Mon": semDateTime,
-    "Tue": semDateTime + datetime.timedelta(days=1),
-    "Wed": semDateTime + datetime.timedelta(days=2),
-    "Thu": semDateTime + datetime.timedelta(days=3),
-    "Fri": semDateTime + datetime.timedelta(days=4),
-    "Sat": semDateTime + datetime.timedelta(days=5),
-    "Sun": semDateTime + datetime.timedelta(days=6)
+    "Mon": semStartDateTime,
+    "Tue": semStartDateTime + datetime.timedelta(days=1),
+    "Wed": semStartDateTime + datetime.timedelta(days=2),
+    "Thu": semStartDateTime + datetime.timedelta(days=3),
+    "Fri": semStartDateTime + datetime.timedelta(days=4),
+    "Sat": semStartDateTime + datetime.timedelta(days=5),
+    "Sun": semStartDateTime + datetime.timedelta(days=6)
 }
 
 class Lesson:
@@ -63,11 +78,13 @@ for row in table:
         string = list(child[0].stripped_strings)[0].split(" ")
         venue = list(child[1].stripped_strings)[0].split(",")[0]
         lecturer = list(child[2].stripped_strings)[0]
-    test = list(child[2].stripped_strings)
     lesson = Lesson(string[0], string[31] + " " + string[32], string[34] + " " + string[35], venue, lecturer, string[37][:-1])
     subjects[i].lesson.append(lesson)
-
-fn = cwd + "/" + (sys.argv[1][:-5] + ".csv")
+if len(sys.argv) == 3:
+    if sys.argv[2] == "node":
+        fn = cwd + "/" + (sys.argv[1][:-5] + ".csv")
+else:
+    fn = "timetable.csv"
 with open(fn, 'w', newline='') as csvFile:
     writer = csv.writer(csvFile)
     writer.writerow(['Subject', 'Start Date', 'Start Time', 'End Date', 'End Time', 'Location'])
@@ -75,7 +92,7 @@ with open(fn, 'w', newline='') as csvFile:
         subjectName = subject.name
         for lesson in subject.lesson:
             date = dayDict[lesson.day]
-            for x in range(7):
+            for x in range(semDuration):
                 writer.writerow([subjectName + " (" + lesson.type + ")", date.strftime("%m/%d/%Y"),
                                  lesson.starttime, date.strftime("%m/%d/%Y"), lesson.endtime, lesson.venue])
                 date += datetime.timedelta(days=7)
